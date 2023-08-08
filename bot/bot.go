a69358e0dffe0101ffb6e338643d607dd369b596
Ayan Maiti <100613640+moonman369@users.noreply.github.com>
2023-08-08 15:40:38 +0530

    Minor changes

--- bot/bot.go
+++ bot/bot.go
@@ -69,8 +69,6 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}()
 	Resp := gpt.SendPrompt(fmt.Sprintf("Refer to your self as Ping-Bot-v0 whenever you are asked to identify yourself. %v", m.Content))
 
-	fmt.Println(Resp)
-
 	if len(Resp.Choices) < 1 {
 		s.ChannelMessageSend(m.ChannelID, "Could not create suitable response. Please try again.")
 		return
@@ -82,6 +80,6 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	fmt.Println(Resp.Choices[0].Message.Content)
 
-	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf(`%v`, Resp.Choices[0].Message.Content))
+	s.ChannelMessageSend(m.ChannelID, fmt.Sprintf(`%v`, string(Resp.Choices[0].Message.Content)))
 
 }
